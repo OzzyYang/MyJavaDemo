aec0eafba54e34193184e3b0625d59247eb8b583
OzzyYang <freefantasystation@163.com>
2021-04-16 14:21:29 +0800

    排序：冒泡排序，乱序排序，插入排序，选择排序

--- Algorithm/Sort/Sort.java
+++ Algorithm/Sort/Sort.java
@@ -9,12 +9,8 @@ package Sort;
 public class Sort {
     public static void main(String[] args) {
         int[] a = {25, 24, 12, 76, 98, 101, 90, 28, 56, 56, 4, 20, 465, 120, 845, 1, 6};
-        Sort sort = new Sort();
-        sort.bubbleSort(a);
-        sort.printArray(a);
-        sort.ranDomSort(a);
-        sort.insertionSort(a);
-        sort.printArray(a);
+        selectionSort(a, "ASC");
+        printArray(a);
     }
 
     /**
@@ -22,7 +18,7 @@ public class Sort {
      *
      * @param arr 需要排序的数组
      */
-    public void insertionSort(int[] arr) {
+    public static void insertionSort(int[] arr) {
         int cacheNum = 0;
         for (int i = 1; i < arr.length; i++) {
             cacheNum = arr[i];
@@ -42,7 +38,7 @@ public class Sort {
      *
      * @param arr 需要排序的数组
      */
-    public void bubbleSort(int[] arr) {
+    public static void bubbleSort(int[] arr) {
         if (arr == null || arr.length <= 1) return;
         int canum = 0;// 交换数值时的缓存变量
         // 冒泡排序
@@ -58,11 +54,71 @@ public class Sort {
     }
 
     /**
+     * 按照指定的顺序对数组进行冒泡排序
+     *
+     * @param array     需要排序的数组
+     * @param sortOrder ASC表示升序 DES表示降序
+     */
+    public static void bubbleSort(int[] array, String sortOrder) {
+        int canum = 0;// 交换数值时的缓存变量
+        // 冒泡排序
+        if (sortOrder == "ASC") {
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = 0; j < array.length - 1; j++) {
+                    if (array[j] > array[j + 1]) {
+                        canum = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = canum;
+                    }
+                }
+            }
+        } else if (sortOrder == "DES") {
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = 0; j < array.length - 1; j++) {
+                    if (array[j] < array[j + 1]) {
+                        canum = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = canum;
+                    }
+                }
+            }
+
+        }
+    }
+
+    /**
+     * 按照指定的顺序对数组进行选择排序
+     *
+     * @param array     需要排序的数组
+     * @param sortOrder ASC表示升序 DES表示降序
+     */
+    public static void selectionSort(int[] array, String sortOrder) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int index = i;
+            for (int j = i; j < array.length; j++) {
+                if (sortOrder.equals("ASC")) {
+                    if (array[index] > array[j]) index = j;
+                } else if (sortOrder.equals("DES")) {
+                    if (array[index] < array[j]) index = j;
+                } else {
+                    System.out.println("sortOrder ERROR!");
+                    return;
+                }
+            }
+            if (index != i) {
+                int cache = array[i];
+                array[i] = array[index];
+                array[index] = cache;
+            }
+        }
+    }
+
+    /**
      * 打乱数组的顺序
      *
      * @param arr 需要打乱顺序的数组
      */
-    public void ranDomSort(int[] arr) {
+    public static void randomSort(int[] arr) {
         if (arr == null || arr.length <= 1) return;
         int canum = 0;// 交换数值时的缓存变量
         for (int i = 0; i < arr.length - 1; i++) {
@@ -79,7 +135,7 @@ public class Sort {
      *
      * @param arr 要输出的数组
      */
-    public void printArray(int[] arr) {
+    public static void printArray(int[] arr) {
         if (arr == null || arr.length == 0) return;
         int i = 0;
         for (int element : arr) {
@@ -87,5 +143,34 @@ public class Sort {
             i++;
         }
         System.out.println();
+    }
+
+
+    /**
+     * 查找数组的最大值
+     *
+     * @param array 要查找的数组
+     * @return 最大值
+     */
+    public static int arrayMaximun(int[] array) {
+        int max = array[0];
+        for (int element : array) {
+            max = element > max ? element : max;
+        }
+        return max;
+    }
+
+    /**
+     * 查找数组的最小值
+     *
+     * @param array 要查找的数组
+     * @return 最小值
+     */
+    public static int arrayMinimun(int[] array) {
+        int min = array[0];
+        for (int element : array) {
+            min = element < min ? element : min;
+        }
+        return min;
     }
 }
